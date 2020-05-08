c2964a5e7f0ec9ee5eb2448b94fe42d1fcd570bc
Arman Parastaran <arman.parastaran@me.com>
2020-05-07 20:16:27 -0400

    added creating/searching/adding

--- MusiqNode/routes/spotify.router.js
+++ MusiqNode/routes/spotify.router.js
@@ -7,27 +7,74 @@ var SpotifyWebApi = require('spotify-web-api-node');
 var spotifyApi = new SpotifyWebApi();
 var userid = 'arman.parastaran';
 
-var accessToken = 'BQCTGZs0EynKBkofKcIhxvwy7P2AwDXX4dBtGU18MBGdJCemuvZt__FC2mbmBZjDGu5RgwR_8pblmS-iEMwK-DInJiZiCLSLdCO8voMRgXTXRmNJ-lfO5srlq4G9WhV4hOElCaQT_eCJSlj4lavQpNARTEnS_hm1QzH0ZVtdgmTLkbvavNluJJPpdJqiNBvxo2WGjrdqzufqHd4KA9FGKDYyjS5Eqvca';
-//Set access token in node
+var accessToken = 'BQBcQXshskc2BeBdKCdGOHPIziwWR0Gb4o6Q8-nXWlbN-Cm9_SdBZRXV3IZ_4w090hjaD1W0s0vtAcCYobE4T3fGnUvG_JqCSqRPlerYCgY_d8-J2MLrr706cXM9PiKkM8ONwNqAm46OlYzuJzI8ub_-EeKtYiiAKbqn6C2R822Ahe4qABBjBmnjX7RKLweJ4_lFzMp91HFyJrikMIuxyel-4ONrrmsv';
 spotifyApi.setAccessToken(accessToken);
 
 router.use(express.static(__dirname + '/public'))
   .use(cors())
   .use(cookieParser());
 
-router.post('/createplaylist',
-  function () {spotifyApi.createPlaylist(
-    userid,
-    'Test Playlist 1',
+router.post('/createplaylist/:name',
+  function (req, res) {spotifyApi.createPlaylist(userid, req.params.name,
     function(err, data) {
       if (err) {
         console.error('Something went wrong!');
-      } else {
+        return res.status(400).json({
+          message: 'Something went wrong'
+        });
+      }
+      else {
         console.log(data.body);
-        return data.body.uri;
+        return res.status(200).json({
+          message: 'Playlist created successfully',
+          uri: data.body.uri
+        })
       }
     }
   );}
 );
+
+router.get('/search/:name/:artist',
+  function (req, res) {
+    spotifyApi.searchTracks('track:' + req.params.name + ' artist:' + req.params.artist, {limit:1, offset:0},
+      function (err, data){
+      if (err) {
+        console.error('Something went wrong!');
+        return res.status(400).json({
+          message: err
+        });
+      }
+      else {
+        console.log(data.body);
+        return res.status(200).json({
+          message: 'Song searched successfully, found',
+          uri: data.body.tracks.items[0].uri
+        })
+      }
+    }
+    );
+});
+
+
+
+router.post('/addtoplaylist/:playlist/:track',
+  function (req, res) {
+    spotifyApi.addTracksToPlaylist('5fxA9mViLWA7pJVbnahinb', [req.params.track], '',
+    function (err, data){
+      if (err) {
+        console.error('Something went wrong!');
+        return res.status(400).json({
+          message: err
+        });
+      }
+      else {
+        console.log(data.body);
+        return res.status(200).json({
+          message: 'Song successfully added to playlist'
+        });
+      }
+    }
+    );
+});
 
 module.exports = router;
